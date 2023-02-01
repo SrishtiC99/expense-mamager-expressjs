212dd364a8c74c741045fa9f19a1e422894d3d78
SrishtiC99 <iamsri9631@gmail.com>
2023-02-01 19:32:42 +0530

    all api requests added

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,27 +1,59 @@
 const Entry =  require('../models/Entry');
-const getAllEntries = (req, res) => {
-  res.send("get All Entries");
-}
-
-const getEntry = (req, res) => {
-  res.json({id: req.params.id});
-}
-
-const createEntry = async (req, res) => {
+const getAllEntries = async (req, res) => {
   try {
-    const entry = await Entry.create(req.body);
-    res.status(201).send({entry});
+    const entries = await Entry.find({});
+    res.status(201).json({entries});
   } catch (err) {
     res.json({msg: err});
   }
 }
 
-const updateEntry = (req, res) => {
-  res.send("Update Entry");
+const getEntry = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const entry = await Entry.findById(id);
+    if(!entry) {
+      return res.status(404).json(`entry with id: ${id} not found`);
+    }
+    res.status(201).json({entry});
+  } catch (err) {
+    res.json({msg: err});
+  }
 }
 
-const deleteEntry = (req, res) => {
-  res.send("Delete Entry");
+const createEntry = async (req, res) => {
+  try {
+    const entry = await Entry.create(req.body);
+    res.status(201).json({entry});
+  } catch (err) {
+    res.json({msg: err});
+  }
+}
+
+const updateEntry = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const entry = await Entry.findOneAndUpdate({_id: id}, req.body, {new: true, runValidators: true});
+    if(!entry) {
+      return res.status(404).json(`entry with id: ${id} not found`);
+    }
+    res.status(201).json({entry});
+  } catch (err) {
+    res.json({msg: err});
+  }
+}
+
+const deleteEntry = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const entry = await Entry.deleteOne({_id: id});
+    if(!entry.deletedCount) {
+      return res.status(404).json(`entry with id: ${id} not found`);
+    }
+    res.status(201).json({entry});
+  } catch (err) {
+    res.json({msg: err});
+  }
 }
 
 module.exports = {
